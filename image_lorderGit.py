63d79679b944096d340739d16d2c0a4abf121ecf
Jose <jose.r@suburbanstreettrading.com>
2022-12-21 11:34:26 -0500

    ASI_images

--- image_lorderGit.py
+++ image_lorderGit.py
@@ -1,30 +1,48 @@
 import csv
-import io
-import urllib.request
 import requests  # http://docs.python-requests.org/en/master/
+import shutil # to save it locally
 
 
 
 
 
-
+import time
 # example image url: https://m.media-amazon.com/images/S/aplus-media/vc/6a9569ab-cb8e-46d9-8aea-a7022e58c74a.jpg
 def download_image(name ,image_url):
-    request =   requests.get(image_url)
-    file_location = f'ASI_Images/{name}.jpg'
-    print(file_location)
-    with open(f'ASI_Images/{name}.jpg', 'wb') as f:
-        f.write(request.content)
+    try:
+        request =   requests.get(image_url, timeout=5)
+        print(request)
+        # Check if the image was retrieved successfully
+        if request.status_code == 200:
+            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            request.raw.decode_content = True
+
+            file_location = f'ASI_Images/{name}.jpg'
+            print(file_location, request.status_code)
+            with open(file_location, 'wb') as f:
+                shutil.copyfileobj(request.raw, f)
+                f.close()
+    except:
+        fail_List=[]
+        fail_List.append((name,image_url))
+
 
 
 csv_path = 'ASI_images.csv'
 csv_block = csv.reader(open(csv_path,encoding='UTF-8'))
 
+
+count = 0
 for item in csv_block:
     if item[0] == '﻿SKU':
         continue
+    count +=1
     item_number = item[0]
     #print(item_number)
+    if item[6] =='':
+        continue
     product_URL = item[6]
     #print(product_URL)
     download_image(item_number,product_URL)
+    print(count)
+
